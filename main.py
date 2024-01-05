0da69488af50642e102e27d0bfc77cf6885bac32
vikashPirth <vikash.pirthiani@gmail.com>
2024-01-05 17:51:38 +0500

    backround and assets

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ white_color = (255, 255, 255)
 game_window = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
+background_image = pygame.image.load('assets/background.png')
+background = pygame.transform.scale(background_image, (width, height))
+
 def game_loop():
     while True:
+        # handle events 
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 return
         
         game_window.fill(white_color)
+        game_window.blit(background, (0,0))
+        
         pygame.display.update()
 
         clock.tick(60)
